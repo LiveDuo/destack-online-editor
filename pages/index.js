a8b0d36f8cae4a126d9aff2a867954f41dad5d11
Andreas Tzionis <andy0n7@gmail.com>
2021-04-28 22:14:05 +0300

    removed unused files, deleted getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import { ContentProvider } from 'destack'
 import 'grapesjs/dist/css/grapes.min.css'
 
-export { getStaticProps } from 'destack/build/server'
-
 export default function Page(props) { return <ContentProvider {...props} server={false}/> }
