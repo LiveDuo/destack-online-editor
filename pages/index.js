6950a55d848960e31e95a1342598c1445b379729
Andreas Tzionis <andy0n7@gmail.com>
2021-06-20 20:52:00 +0300

    updated destack

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 import { ContentProvider } from 'destack'
 import 'grapesjs/dist/css/grapes.min.css'
 
-export default function Page(props) { return <ContentProvider {...props} server={false}/> }
+export default function Page(props) { return <ContentProvider {...props} showEditorInProd={true}/> }
